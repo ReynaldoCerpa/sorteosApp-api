b8b588659e2686f68ad3e72d26801cf26d40d48e
sayuri <sayuri.ramos.h@gmail.com>
2021-11-22 11:21:44 -0800

    Controller y route de Carteras

--- src/controllers/carteras.controller.ts
+++ src/controllers/carteras.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import { connect } from '../database'
 const path = require("path");
 
+/*
 export async function getCarteras(req: Request, res: Response): Promise<Response | void> {
     try {
         //const idPromotor = req.body.idPromotor;
@@ -12,5 +13,29 @@ export async function getCarteras(req: Request, res: Response): Promise<Response
     }
     catch (e) {
         console.log(e)
+    }
+
+
+
+    const hasValue = JSON.stringify(accounts);        
+        
+        const validation = (hasValue.indexOf("idPromotor") == 5) ? "true" : "false";
+        return res.send(validation);
+}*/
+
+export async function getCarteras(req: Request, res: Response){
+    try{
+
+        var id = req.body.idColaborador;
+        console.log(req.body.idColaborador);
+        
+        console.log("Params: ", req.body);
+        
+        const conn = await connect();
+        const posts = await conn.query('SELECT * FROM cartera where idColaborador = ?',[id]); 
+
+        return res.json(posts[0]);
+    } catch (e) {
+        console.log(e);
     }
 }
--- src/routes/carteras.routes.ts
+++ src/routes/carteras.routes.ts
@@ -3,7 +3,7 @@ const router = Router();
 
 import * as index from "../controllers/carteras.controller"
 router.route("/carteras")
-    .get(index.getCarteras)
+    .post(index.getCarteras)
 
     
 export default router;
