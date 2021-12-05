383cb8089dc03cf95e7d50415893043fa8b19440
sayuri <sayuri.ramos.h@gmail.com>
2021-12-05 01:38:59 -0800

    agregar para sacar total abono

--- src/controllers/totalabono.controller.js
+++ src/controllers/totalabono.controller.js
@@ -1,20 +1,18 @@
-import { Request, Response } from "express";
+import { json, Request, Response } from "express";
 import { connect } from '../database'
 const path = require("path");
 
-export async function getTotalAbono(req: Request, res: Response): Promise<Response | void> {
+export async function getTotalAbono(req, res ) {
     try {
         var id = req.body.numBoleto;
         console.log("Params: ", req.body);
 
             const conn = await connect();
             const posts = await conn.query('select sum(cantidad) as cantidad from abono where numBoleto = ?', [id]);
-    
+          
+            console.log(posts[0]);
             return res.json(posts[0]);
 
-
-
-       
     } catch (e) {
         console.log(e);
     }
